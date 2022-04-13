df3e44eeb209cc22efb85dd5d3c319f9019d3bcc
hyrious <hyrious@users.noreply.github.com>
2022-04-13 10:42:56 +0000

    deploy: c17c0a6ad38c198ec4b6304b4b3d360a417429ca

---
+++ BuildOptions-5YX22LY6.js
@@ -0,0 +1,138 @@
+import{a as ys,b as f}from"./chunk-I45SB526.js";import{A as n,B as s,C as ft,E as _,F as gs,G as t,H as i,I as r,J as a,K as gt,L as ms,M as ue,N as Ml,Q as zl,R as ks,k as Rl,ka as bs,la as Es,m as vs,o as ds,p as ql,u as _s,w as e,x as fs,y as o}from"./chunk-WSAFY2JV.js";import"./chunk-ON5OQYWL.js";function As(l){let O,D,Z,he,u,q,ze,oe,Fe,p,Ge,ie,Je,Ke,ce,Ue,R,Qe,M,We,$,pe=l[2].bundle+"",we,Xe,Ye,m,Ze,k,$e,x,H,mt,E,kt,ve,bt,Et,y,yt,de,Lt,It,At,F,jt,ee,Nt,_e,xe=l[2].minify+"",rt,Pt,Tt,b,Bt,L,Ut,fe,wt,Ot,I,Dt,ge,Vt,Ct,A,St,me,Ht,Rt,qt,d,Mt,j,Ls,zt,ke,Ft,Gt,N,Is,Jt,be,Kt,Qt,P,Wt,Ee,Xt,Yt,T,Zt,ye,$t,xt,B,el,Le,tl,ll,sl,G,al,te,nl,Ie,et=l[2].splitting+"",ut,rl,ul,J,ol,z,il,le,hl,tt=l[2].target+"",ot,cl,pl,vl,V,dl,U,_l,Ae,fl,gl,w,ml,je,kl,bl,El,K,yl,se,Ll,Ne,lt=l[2].ignoreAnnotations+"",it,Il,Al,Q,jl,ae,Nl,Pe,st=l[2].keepNames+"",ht,Pl,Tl,W,Bl,ne,Ul,Te,at=l[2].sourcesContent+"",ct,wl,Ol,X,Dl,re,Vl,Be,nt=l[2].treeShaking+"",pt,Cl,Sl,vt,dt,Hl,Gl;return{c(){O=n("article"),D=n("header"),Z=n("h2"),he=s("Build Options"),u=ft(),q=n("i"),ze=ft(),oe=n("i"),Fe=ft(),p=n("pre"),Ge=s("result = "),ie=n("span"),Je=s("await"),Ke=s(` esbuild.build({
+    entryPoints: `),ce=new ks,Ue=s(`,
+    bundle: `),R=n("div"),Qe=s(`
+      `),M=n("input"),We=s(`
+      `),$=n("label"),we=s(pe),Xe=s(`
+    `),Ye=s(`,
+    format: `),m=n("div"),Ze=s(`
+      `),k=n("input"),$e=s(`
+      `),x=n("label"),H=s('"esm"'),mt=s(`
+      `),E=n("input"),kt=s(`
+      `),ve=n("label"),bt=s('"cjs"'),Et=s(`
+      `),y=n("input"),yt=s(`
+      `),de=n("label"),Lt=s('"iife"'),It=s(`
+    `),At=s(`,
+    minify: `),F=n("div"),jt=s(`
+      `),ee=n("input"),Nt=s(`
+      `),_e=n("label"),rt=s(xe),Pt=s(`
+    `),Tt=s(`,
+    platform: `),b=n("div"),Bt=s(`
+      `),L=n("input"),Ut=s(`
+      `),fe=n("label"),wt=s('"browser"'),Ot=s(`
+      `),I=n("input"),Dt=s(`
+      `),ge=n("label"),Vt=s('"node"'),Ct=s(`
+      `),A=n("input"),St=s(`
+      `),me=n("label"),Ht=s('"neutral"'),Rt=s(`
+    `),qt=s(`,
+    sourcemap: `),d=n("div"),Mt=s(`
+      `),j=n("input"),zt=s(`
+      `),ke=n("label"),Ft=s("true"),Gt=s(`
+      `),N=n("input"),Jt=s(`
+      `),be=n("label"),Kt=s("false"),Qt=s(`
+      `),P=n("input"),Wt=s(`
+      `),Ee=n("label"),Xt=s('"inline"'),Yt=s(`
+      `),T=n("input"),Zt=s(`
+      `),ye=n("label"),$t=s('"external"'),xt=s(`
+      `),B=n("input"),el=s(`
+      `),Le=n("label"),tl=s('"both"'),ll=s(`
+    `),sl=s(`,
+    splitting: `),G=n("div"),al=s(`
+      `),te=n("input"),nl=s(`
+      `),Ie=n("label"),ut=s(et),rl=s(`
+    `),ul=s(`,
+    target: `),J=n("div"),ol=s(`
+      `),z=n("input"),il=s(`
+      `),le=n("label"),hl=s('"'),ot=s(tt),cl=s('"'),pl=s(`
+    `),vl=s(`,
+    charset: `),V=n("div"),dl=s(`
+      `),U=n("input"),_l=s(`
+      `),Ae=n("label"),fl=s('"utf8"'),gl=s(`
+      `),w=n("input"),ml=s(`
+      `),je=n("label"),kl=s('"ascii"'),bl=s(`
+    `),El=s(`,
+    ignoreAnnotations: `),K=n("div"),yl=s(`
+      `),se=n("input"),Ll=s(`
+      `),Ne=n("label"),it=s(lt),Il=s(`
+    `),Al=s(`,
+    keepNames: `),Q=n("div"),jl=s(`
+      `),ae=n("input"),Nl=s(`
+      `),Pe=n("label"),ht=s(st),Pl=s(`
+    `),Tl=s(`,
+    sourcesContent: `),W=n("div"),Bl=s(`
+      `),ne=n("input"),Ul=s(`
+      `),Te=n("label"),ct=s(at),wl=s(`
+    `),Ol=s(`,
+    treeShaking: `),X=n("div"),Dl=s(`
+      `),re=n("input"),Vl=s(`
+      `),Be=n("label"),pt=s(nt),Cl=s(`
+    `),Sl=s(`,
+})`),this.h()},l(h){O=r(h,"ARTICLE",{"aria-expanded":!0,class:!0});var c=i(O);D=r(c,"HEADER",{class:!0});var Oe=i(D);Z=r(Oe,"H2",{class:!0});var Jl=i(Z);he=a(Jl,"Build Options"),Jl.forEach(o),u=gt(Oe),q=r(Oe,"I",{class:!0,title:!0}),i(q).forEach(o),ze=gt(Oe),oe=r(Oe,"I",{class:!0,title:!0}),i(oe).forEach(o),Oe.forEach(o),Fe=gt(c),p=r(c,"PRE",{style:!0,class:!0});var v=i(p);Ge=a(v,"result = "),ie=r(v,"SPAN",{class:!0});var Kl=i(ie);Je=a(Kl,"await"),Kl.forEach(o),Ke=a(v,` esbuild.build({
+    entryPoints: `),ce=ms(v),Ue=a(v,`,
+    bundle: `),R=r(v,"DIV",{class:!0});var De=i(R);Qe=a(De,`
+      `),M=r(De,"INPUT",{id:!0,type:!0,class:!0}),We=a(De,`
+      `),$=r(De,"LABEL",{for:!0,class:!0});var Ql=i($);we=a(Ql,pe),Ql.forEach(o),Xe=a(De,`
+    `),De.forEach(o),Ye=a(v,`,
+    format: `),m=r(v,"DIV",{class:!0});var C=i(m);Ze=a(C,`
+      `),k=r(C,"INPUT",{type:!0,name:!0,id:!0,class:!0}),$e=a(C,`
+      `),x=r(C,"LABEL",{for:!0,class:!0});var Wl=i(x);H=a(Wl,'"esm"'),Wl.forEach(o),mt=a(C,`
+      `),E=r(C,"INPUT",{type:!0,name:!0,id:!0,class:!0}),kt=a(C,`
+      `),ve=r(C,"LABEL",{for:!0,class:!0});var Xl=i(ve);bt=a(Xl,'"cjs"'),Xl.forEach(o),Et=a(C,`
+      `),y=r(C,"INPUT",{type:!0,name:!0,id:!0,class:!0}),yt=a(C,`
+      `),de=r(C,"LABEL",{for:!0,class:!0});var Yl=i(de);Lt=a(Yl,'"iife"'),Yl.forEach(o),It=a(C,`
+    `),C.forEach(o),At=a(v,`,
+    minify: `),F=r(v,"DIV",{class:!0});var Ve=i(F);jt=a(Ve,`
+      `),ee=r(Ve,"INPUT",{id:!0,type:!0,class:!0}),Nt=a(Ve,`
+      `),_e=r(Ve,"LABEL",{for:!0,class:!0});var Zl=i(_e);rt=a(Zl,xe),Zl.forEach(o),Pt=a(Ve,`
+    `),Ve.forEach(o),Tt=a(v,`,
+    platform: `),b=r(v,"DIV",{class:!0});var S=i(b);Bt=a(S,`
+      `),L=r(S,"INPUT",{type:!0,name:!0,id:!0,class:!0}),Ut=a(S,`
+      `),fe=r(S,"LABEL",{for:!0,class:!0});var $l=i(fe);wt=a($l,'"browser"'),$l.forEach(o),Ot=a(S,`
+      `),I=r(S,"INPUT",{type:!0,name:!0,id:!0,class:!0}),Dt=a(S,`
+      `),ge=r(S,"LABEL",{for:!0,class:!0});var xl=i(ge);Vt=a(xl,'"node"'),xl.forEach(o),Ct=a(S,`
+      `),A=r(S,"INPUT",{type:!0,name:!0,id:!0,class:!0}),St=a(S,`
+      `),me=r(S,"LABEL",{for:!0,class:!0});var es=i(me);Ht=a(es,'"neutral"'),es.forEach(o),Rt=a(S,`
+    `),S.forEach(o),qt=a(v,`,
+    sourcemap: `),d=r(v,"DIV",{class:!0});var g=i(d);Mt=a(g,`
+      `),j=r(g,"INPUT",{type:!0,name:!0,id:!0,class:!0}),zt=a(g,`
+      `),ke=r(g,"LABEL",{for:!0,class:!0});var ts=i(ke);Ft=a(ts,"true"),ts.forEach(o),Gt=a(g,`
+      `),N=r(g,"INPUT",{type:!0,name:!0,id:!0,class:!0}),Jt=a(g,`
+      `),be=r(g,"LABEL",{for:!0,class:!0});var ls=i(be);Kt=a(ls,"false"),ls.forEach(o),Qt=a(g,`
+      `),P=r(g,"INPUT",{type:!0,name:!0,id:!0,class:!0}),Wt=a(g,`
+      `),Ee=r(g,"LABEL",{for:!0,class:!0});var ss=i(Ee);Xt=a(ss,'"inline"'),ss.forEach(o),Yt=a(g,`
+      `),T=r(g,"INPUT",{type:!0,name:!0,id:!0,class:!0}),Zt=a(g,`
+      `),ye=r(g,"LABEL",{for:!0,class:!0});var as=i(ye);$t=a(as,'"external"'),as.forEach(o),xt=a(g,`
+      `),B=r(g,"INPUT",{type:!0,name:!0,id:!0,class:!0}),el=a(g,`
+      `),Le=r(g,"LABEL",{for:!0,class:!0});var ns=i(Le);tl=a(ns,'"both"'),ns.forEach(o),ll=a(g,`
+    `),g.forEach(o),sl=a(v,`,
+    splitting: `),G=r(v,"DIV",{class:!0});var Ce=i(G);al=a(Ce,`
+      `),te=r(Ce,"INPUT",{id:!0,type:!0,class:!0}),nl=a(Ce,`
+      `),Ie=r(Ce,"LABEL",{for:!0,class:!0});var rs=i(Ie);ut=a(rs,et),rs.forEach(o),rl=a(Ce,`
+    `),Ce.forEach(o),ul=a(v,`,
+    target: `),J=r(v,"DIV",{class:!0});var Se=i(J);ol=a(Se,`
+      `),z=r(Se,"INPUT",{id:!0,type:!0,placeholder:!0,class:!0}),il=a(Se,`
+      `),le=r(Se,"LABEL",{for:!0,class:!0});var _t=i(le);hl=a(_t,'"'),ot=a(_t,tt),cl=a(_t,'"'),_t.forEach(o),pl=a(Se,`
+    `),Se.forEach(o),vl=a(v,`,
+    charset: `),V=r(v,"DIV",{class:!0});var Y=i(V);dl=a(Y,`
+      `),U=r(Y,"INPUT",{type:!0,name:!0,id:!0,class:!0}),_l=a(Y,`
+      `),Ae=r(Y,"LABEL",{for:!0,class:!0});var us=i(Ae);fl=a(us,'"utf8"'),us.forEach(o),gl=a(Y,`
+      `),w=r(Y,"INPUT",{type:!0,name:!0,id:!0,class:!0}),ml=a(Y,`
+      `),je=r(Y,"LABEL",{for:!0,class:!0});var os=i(je);kl=a(os,'"ascii"'),os.forEach(o),bl=a(Y,`
+    `),Y.forEach(o),El=a(v,`,
+    ignoreAnnotations: `),K=r(v,"DIV",{class:!0});var He=i(K);yl=a(He,`
+      `),se=r(He,"INPUT",{id:!0,type:!0,class:!0}),Ll=a(He,`
+      `),Ne=r(He,"LABEL",{for:!0,class:!0});var is=i(Ne);it=a(is,lt),is.forEach(o),Il=a(He,`
+    `),He.forEach(o),Al=a(v,`,
+    keepNames: `),Q=r(v,"DIV",{class:!0});var Re=i(Q);jl=a(Re,`
+      `),ae=r(Re,"INPUT",{id:!0,type:!0,class:!0}),Nl=a(Re,`
+      `),Pe=r(Re,"LABEL",{for:!0,class:!0});var hs=i(Pe);ht=a(hs,st),hs.forEach(o),Pl=a(Re,`
+    `),Re.forEach(o),Tl=a(v,`,
+    sourcesContent: `),W=r(v,"DIV",{class:!0});var qe=i(W);Bl=a(qe,`
+      `),ne=r(qe,"INPUT",{id:!0,type:!0,class:!0}),Ul=a(qe,`
+      `),Te=r(qe,"LABEL",{for:!0,class:!0});var cs=i(Te);ct=a(cs,at),cs.forEach(o),wl=a(qe,`
+    `),qe.forEach(o),Ol=a(v,`,
+    treeShaking: `),X=r(v,"DIV",{class:!0});var Me=i(X);Dl=a(Me,`
+      `),re=r(Me,"INPUT",{id:!0,type:!0,class:!0}),Vl=a(Me,`
+      `),Be=r(Me,"LABEL",{for:!0,class:!0});var ps=i(Be);pt=a(ps,nt),ps.forEach(o),Cl=a(Me,`
+    `),Me.forEach(o),Sl=a(v,`,
+})`),v.forEach(o),c.forEach(o),this.h()},h(){t(Z,"class","svelte-1hvesgk"),t(q,"class","i-mdi-reload svelte-1hvesgk"),t(q,"title","Reset Build Options"),t(oe,"class","i-mdi-information svelte-1hvesgk"),t(oe,"title","not all options are supported, try use esbuild in the browser console or install it locally."),t(D,"class","svelte-1hvesgk"),t(ie,"class","hljs-keyword"),ce.a=Ue,t(M,"id","opt-bundle"),t(M,"type","checkbox"),t(M,"class","svelte-1hvesgk"),t($,"for","opt-bundle"),t($,"class","hljs-keyword svelte-1hvesgk"),t(R,"class","option svelte-1hvesgk"),t(k,"type","radio"),t(k,"name","format"),t(k,"id","opt-format-esm"),k.__value="esm",k.value=k.__value,t(k,"class","svelte-1hvesgk"),l[8][0].push(k),t(x,"for","opt-format-esm"),t(x,"class","hljs-string svelte-1hvesgk"),t(E,"type","radio"),t(E,"name","format"),t(E,"id","opt-format-cjs"),E.__value="cjs",E.value=E.__value,t(E,"class","svelte-1hvesgk"),l[8][0].push(E),t(ve,"for","opt-format-cjs"),t(ve,"class","hljs-string svelte-1hvesgk"),t(y,"type","radio"),t(y,"name","format"),t(y,"id","opt-format-iife"),y.__value="iife",y.value=y.__value,t(y,"class","svelte-1hvesgk"),l[8][0].push(y),t(de,"for","opt-format-iife"),t(de,"class","hljs-string svelte-1hvesgk"),t(m,"class","option svelte-1hvesgk"),t(ee,"id","opt-minify"),t(ee,"type","checkbox"),t(ee,"class","svelte-1hvesgk"),t(_e,"for","opt-minify"),t(_e,"class","hljs-keyword svelte-1hvesgk"),t(F,"class","option svelte-1hvesgk"),t(L,"type","radio"),t(L,"name","platform"),t(L,"id","opt-platform-browser"),L.__value="browser",L.value=L.__value,t(L,"class","svelte-1hvesgk"),l[8][1].push(L),t(fe,"for","opt-platform-browser"),t(fe,"class","hljs-string svelte-1hvesgk"),t(I,"type","radio"),t(I,"name","platform"),t(I,"id","opt-platform-node"),I.__value="node",I.value=I.__value,t(I,"class","svelte-1hvesgk"),l[8][1].push(I),t(ge,"for","opt-platform-node"),t(ge,"class","hljs-string svelte-1hvesgk"),t(A,"type","radio"),t(A,"name","platform"),t(A,"id","opt-platform-neutral"),A.__value="neutral",A.value=A.__value,t(A,"class","svelte-1hvesgk"),l[8][1].push(A),t(me,"for","opt-platform-neutral"),t(me,"class","hljs-string svelte-1hvesgk"),t(b,"class","option svelte-1hvesgk"),t(j,"type","radio"),t(j,"name","sourcemap"),t(j,"id","opt-sourcemap-true"),j.__value=Ls=!0,j.value=j.__value,t(j,"class","svelte-1hvesgk"),l[8][2].push(j),t(ke,"for","opt-sourcemap-true"),t(ke,"class","hljs-keyword svelte-1hvesgk"),t(N,"type","radio"),t(N,"name","sourcemap"),t(N,"id","opt-sourcemap-false"),N.__value=Is=!1,N.value=N.__value,t(N,"class","svelte-1hvesgk"),l[8][2].push(N),t(be,"for","opt-sourcemap-false"),t(be,"class","hljs-keyword svelte-1hvesgk"),t(P,"type","radio"),t(P,"name","sourcemap"),t(P,"id","opt-sourcemap-inline"),P.__value="inline",P.value=P.__value,t(P,"class","svelte-1hvesgk"),l[8][2].push(P),t(Ee,"for","opt-sourcemap-inline"),t(Ee,"class","hljs-string svelte-1hvesgk"),t(T,"type","radio"),t(T,"name","sourcemap"),t(T,"id","opt-sourcemap-external"),T.__value="external",T.value=T.__value,t(T,"class","svelte-1hvesgk"),l[8][2].push(T),t(ye,"for","opt-sourcemap-external"),t(ye,"class","hljs-string svelte-1hvesgk"),t(B,"type","radio"),t(B,"name","sourcemap"),t(B,"id","opt-sourcemap-both"),B.__value="both",B.value=B.__value,t(B,"class","svelte-1hvesgk"),l[8][2].push(B),t(Le,"for","opt-sourcemap-both"),t(Le,"class","hljs-string svelte-1hvesgk"),t(d,"class","option svelte-1hvesgk"),t(te,"id","opt-splitting"),t(te,"type","checkbox"),t(te,"class","svelte-1hvesgk"),t(Ie,"for","opt-splitting"),t(Ie,"class","hljs-keyword svelte-1hvesgk"),t(G,"class","option svelte-1hvesgk"),t(z,"id","opt-target"),t(z,"type","text"),t(z,"placeholder","esnext"),t(z,"class","svelte-1hvesgk"),t(le,"for","opt-target"),t(le,"class","hljs-hidden svelte-1hvesgk"),t(J,"class","option svelte-1hvesgk"),t(U,"type","radio"),t(U,"name","charset"),t(U,"id","opt-charset-utf8"),U.__value="utf8",U.value=U.__value,t(U,"class","svelte-1hvesgk"),l[8][3].push(U),t(Ae,"for","opt-charset-utf8"),t(Ae,"class","hljs-string svelte-1hvesgk"),t(w,"type","radio"),t(w,"name","charset"),t(w,"id","opt-charset-ascii"),w.__value="ascii",w.value=w.__value,t(w,"class","svelte-1hvesgk"),l[8][3].push(w),t(je,"for","opt-charset-ascii"),t(je,"class","hljs-string svelte-1hvesgk"),t(V,"class","option svelte-1hvesgk"),t(se,"id","opt-ignore-annotations"),t(se,"type","checkbox"),t(se,"class","svelte-1hvesgk"),t(Ne,"for","opt-ignore-annotations"),t(Ne,"class","hljs-keyword svelte-1hvesgk"),t(K,"class","option svelte-1hvesgk"),t(ae,"id","opt-keep-names"),t(ae,"type","checkbox"),t(ae,"class","svelte-1hvesgk"),t(Pe,"for","opt-keep-names"),t(Pe,"class","hljs-keyword svelte-1hvesgk"),t(Q,"class","option svelte-1hvesgk"),t(ne,"id","opt-sources-content"),t(ne,"type","checkbox"),t(ne,"class","svelte-1hvesgk"),t(Te,"for","opt-sources-content"),t(Te,"class","hljs-keyword svelte-1hvesgk"),t(W,"class","option svelte-1hvesgk"),t(re,"id","opt-tree-shaking"),t(re,"type","checkbox"),t(re,"class","svelte-1hvesgk"),t(Be,"for","opt-tree-shaking"),t(Be,"class","hljs-keyword svelte-1hvesgk"),t(X,"class","option svelte-1hvesgk"),t(p,"style",vt=l[0]?"display: none":""),t(p,"class","svelte-1hvesgk"),t(O,"aria-expanded",dt=!l[0]),t(O,"class","svelte-1hvesgk"),zl(O,"expanded",!l[0])},m(h,c){fs(h,O,c),e(O,D),e(D,Z),e(Z,he),e(D,u),e(D,q),e(D,ze),e(D,oe),e(O,Fe),e(O,p),e(p,Ge),e(p,ie),e(ie,Je),e(p,Ke),ce.m(l[1],p),e(p,Ue),e(p,R),e(R,Qe),e(R,M),M.checked=l[2].bundle,e(R,We),e(R,$),e($,we),e(R,Xe),e(p,Ye),e(p,m),e(m,Ze),e(m,k),k.checked=k.__value===l[2].format,e(m,$e),e(m,x),e(x,H),e(m,mt),e(m,E),E.checked=E.__value===l[2].format,e(m,kt),e(m,ve),e(ve,bt),e(m,Et),e(m,y),y.checked=y.__value===l[2].format,e(m,yt),e(m,de),e(de,Lt),e(m,It),e(p,At),e(p,F),e(F,jt),e(F,ee),ee.checked=l[2].minify,e(F,Nt),e(F,_e),e(_e,rt),e(F,Pt),e(p,Tt),e(p,b),e(b,Bt),e(b,L),L.checked=L.__value===l[2].platform,e(b,Ut),e(b,fe),e(fe,wt),e(b,Ot),e(b,I),I.checked=I.__value===l[2].platform,e(b,Dt),e(b,ge),e(ge,Vt),e(b,Ct),e(b,A),A.checked=A.__value===l[2].platform,e(b,St),e(b,me),e(me,Ht),e(b,Rt),e(p,qt),e(p,d),e(d,Mt),e(d,j),j.checked=j.__value===l[2].sourcemap,e(d,zt),e(d,ke),e(ke,Ft),e(d,Gt),e(d,N),N.checked=N.__value===l[2].sourcemap,e(d,Jt),e(d,be),e(be,Kt),e(d,Qt),e(d,P),P.checked=P.__value===l[2].sourcemap,e(d,Wt),e(d,Ee),e(Ee,Xt),e(d,Yt),e(d,T),T.checked=T.__value===l[2].sourcemap,e(d,Zt),e(d,ye),e(ye,$t),e(d,xt),e(d,B),B.checked=B.__value===l[2].sourcemap,e(d,el),e(d,Le),e(Le,tl),e(d,ll),e(p,sl),e(p,G),e(G,al),e(G,te),te.checked=l[2].splitting,e(G,nl),e(G,Ie),e(Ie,ut),e(G,rl),e(p,ul),e(p,J),e(J,ol),e(J,z),Ml(z,l[2].target),e(J,il),e(J,le),e(le,hl),e(le,ot),e(le,cl),e(J,pl),e(p,vl),e(p,V),e(V,dl),e(V,U),U.checked=U.__value===l[2].charset,e(V,_l),e(V,Ae),e(Ae,fl),e(V,gl),e(V,w),w.checked=w.__value===l[2].charset,e(V,ml),e(V,je),e(je,kl),e(V,bl),e(p,El),e(p,K),e(K,yl),e(K,se),se.checked=l[2].ignoreAnnotations,e(K,Ll),e(K,Ne),e(Ne,it),e(K,Il),e(p,Al),e(p,Q),e(Q,jl),e(Q,ae),ae.checked=l[2].keepNames,e(Q,Nl),e(Q,Pe),e(Pe,ht),e(Q,Pl),e(p,Tl),e(p,W),e(W,Bl),e(W,ne),ne.checked=l[2].sourcesContent,e(W,Ul),e(W,Te),e(Te,ct),e(W,wl),e(p,Ol),e(p,X),e(X,Dl),e(X,re),re.checked=l[2].treeShaking,e(X,Vl),e(X,Be),e(Be,pt),e(X,Cl),e(p,Sl),Hl||(Gl=[_(q,"click",gs(l[3])),_(D,"click",l[5]),_(M,"change",l[6]),_(k,"change",l[7]),_(E,"change",l[9]),_(y,"change",l[10]),_(ee,"change",l[11]),_(L,"change",l[12]),_(I,"change",l[13]),_(A,"change",l[14]),_(j,"change",l[15]),_(N,"change",l[16]),_(P,"change",l[17]),_(T,"change",l[18]),_(B,"change",l[19]),_(te,"change",l[20]),_(z,"input",l[21]),_(U,"change",l[22]),_(w,"change",l[23]),_(se,"change",l[24]),_(ae,"change",l[25]),_(ne,"change",l[26]),_(re,"change",l[27])],Hl=!0)},p(h,[c]){c&2&&ce.p(h[1]),c&4&&(M.checked=h[2].bundle),c&4&&pe!==(pe=h[2].bundle+"")&&ue(we,pe),c&4&&(k.checked=k.__value===h[2].format),c&4&&(E.checked=E.__value===h[2].format),c&4&&(y.checked=y.__value===h[2].format),c&4&&(ee.checked=h[2].minify),c&4&&xe!==(xe=h[2].minify+"")&&ue(rt,xe),c&4&&(L.checked=L.__value===h[2].platform),c&4&&(I.checked=I.__value===h[2].platform),c&4&&(A.checked=A.__value===h[2].platform),c&4&&(j.checked=j.__value===h[2].sourcemap),c&4&&(N.checked=N.__value===h[2].sourcemap),c&4&&(P.checked=P.__value===h[2].sourcemap),c&4&&(T.checked=T.__value===h[2].sourcemap),c&4&&(B.checked=B.__value===h[2].sourcemap),c&4&&(te.checked=h[2].splitting),c&4&&et!==(et=h[2].splitting+"")&&ue(ut,et),c&4&&z.value!==h[2].target&&Ml(z,h[2].target),c&4&&tt!==(tt=h[2].target+"")&&ue(ot,tt),c&4&&(U.checked=U.__value===h[2].charset),c&4&&(w.checked=w.__value===h[2].charset),c&4&&(se.checked=h[2].ignoreAnnotations),c&4&&lt!==(lt=h[2].ignoreAnnotations+"")&&ue(it,lt),c&4&&(ae.checked=h[2].keepNames),c&4&&st!==(st=h[2].keepNames+"")&&ue(ht,st),c&4&&(ne.checked=h[2].sourcesContent),c&4&&at!==(at=h[2].sourcesContent+"")&&ue(ct,at),c&4&&(re.checked=h[2].treeShaking),c&4&&nt!==(nt=h[2].treeShaking+"")&&ue(pt,nt),c&1&&vt!==(vt=h[0]?"display: none":"")&&t(p,"style",vt),c&1&&dt!==(dt=!h[0])&&t(O,"aria-expanded",dt),c&1&&zl(O,"expanded",!h[0])},i:Rl,o:Rl,d(h){h&&o(O),l[8][0].splice(l[8][0].indexOf(k),1),l[8][0].splice(l[8][0].indexOf(E),1),l[8][0].splice(l[8][0].indexOf(y),1),l[8][1].splice(l[8][1].indexOf(L),1),l[8][1].splice(l[8][1].indexOf(I),1),l[8][1].splice(l[8][1].indexOf(A),1),l[8][2].splice(l[8][2].indexOf(j),1),l[8][2].splice(l[8][2].indexOf(N),1),l[8][2].splice(l[8][2].indexOf(P),1),l[8][2].splice(l[8][2].indexOf(T),1),l[8][2].splice(l[8][2].indexOf(B),1),l[8][3].splice(l[8][3].indexOf(U),1),l[8][3].splice(l[8][3].indexOf(w),1),Hl=!1,vs(Gl)}}}function js(l,O,D){let Z,he,u;ql(l,ys,H=>D(4,he=H)),ql(l,f,H=>D(2,u=H));let q=!0;function ze(){_s(f,u={bundle:!0},u)}let oe=[[],[],[],[]],Fe=()=>D(0,q=!q);function p(){u.bundle=this.checked,f.set(u)}function Ge(){u.format=this.__value,f.set(u)}function ie(){u.format=this.__value,f.set(u)}function Je(){u.format=this.__value,f.set(u)}function Ke(){u.minify=this.checked,f.set(u)}function ce(){u.platform=this.__value,f.set(u)}function Ue(){u.platform=this.__value,f.set(u)}function R(){u.platform=this.__value,f.set(u)}function Qe(){u.sourcemap=this.__value,f.set(u)}function M(){u.sourcemap=this.__value,f.set(u)}function We(){u.sourcemap=this.__value,f.set(u)}function $(){u.sourcemap=this.__value,f.set(u)}function pe(){u.sourcemap=this.__value,f.set(u)}function we(){u.splitting=this.checked,f.set(u)}function Xe(){u.target=this.value,f.set(u)}function Ye(){u.charset=this.__value,f.set(u)}function m(){u.charset=this.__value,f.set(u)}function Ze(){u.ignoreAnnotations=this.checked,f.set(u)}function k(){u.keepNames=this.checked,f.set(u)}function $e(){u.sourcesContent=this.checked,f.set(u)}function x(){u.treeShaking=this.checked,f.set(u)}return l.$$.update=()=>{if(l.$$.dirty&16){e:D(1,Z=`[${he.filter(H=>H.isEntry).map(H=>H.name).map(H=>`<span class="hljs-string">"${H}"</span>`).join(", ")}]`)}},[q,Z,u,ze,he,Fe,p,Ge,oe,ie,Je,Ke,ce,Ue,R,Qe,M,We,$,pe,we,Xe,Ye,m,Ze,k,$e,x]}var Fl=class extends Es{constructor(O){super(),bs(this,O,js,As,ds,{})}},Us=Fl;export{Us as default};
+//# sourceMappingURL=BuildOptions-5YX22LY6.js.map
