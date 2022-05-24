d359f00ff5b60f5de1992036149752355a1ea556
hyrious <hyrious@outlook.com>
2022-05-24 09:35:49 +0800

    change highlight.js cdn url

--- src/hljs.ts
+++ src/hljs.ts
@@ -1,6 +1,6 @@
 declare var hljs: typeof import("highlight.js").default;
 
-importScripts("https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.4.0/build/highlight.min.js");
+importScripts("https://cdn.jsdelivr.net/npm/@highlightjs/cdn-assets@11.5.1/highlight.min.js");
 
 addEventListener(
   "message",
