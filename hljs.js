f87b9cecf83ab217881de32117271447acf92b78
hyrious <hyrious@users.noreply.github.com>
2023-06-08 10:09:19 +0000

    deploy: 086a9ee69758ac86f10036795c333874a83db687

--- hljs.js
+++ hljs.js
@@ -1,2 +1,2 @@
-importScripts("https://cdn.jsdelivr.net/npm/@highlightjs/cdn-assets@11.5.1/highlight.min.js");addEventListener("message",({data:s})=>{let{id:e,code:t,lang:i}=s,{value:g}=hljs.highlight(t,{language:i||"js"});postMessage({id:e,value:g})});
+importScripts("https://cdn.jsdelivr.net/npm/@highlightjs/cdn-assets@11.8.0/highlight.min.js");addEventListener("message",({data:s})=>{let{id:e,code:t,lang:i}=s,{value:n}=hljs.highlight(t,{language:i||"js"});postMessage({id:e,value:n})});
 //# sourceMappingURL=hljs.js.map
